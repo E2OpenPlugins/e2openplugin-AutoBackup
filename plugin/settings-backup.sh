3157b555c58fa7e3e58eaf7f10336ba7632d4430
Mirakels <mirakels@users.sourceforge.net>
2013-09-04 23:28:40 +0200

    settings-backup updates
    Refactor code.
    Fix grepping for new packages
    Fix autoinstall handling
    Simplify MACADDR handling by setting to nomac if no mac found
    Check if backup dir actually exists as a dir
    
    add root .profile to backup list

--- plugin/settings-backup.sh
+++ plugin/settings-backup.sh
@@ -1,120 +1,93 @@
 #! /bin/sh
 # Backup files to harddisk backup dir
+#
+# Refactor code.
+# Fix grepping for new packages
+# Fix autoinstall handling
+# Simplify MACADDR handling by setting to nomac if no mac found
+# Check if backup dir actually exists as a dir
+
 
 AUTOINSTALL="no"
 
-if [ -z "$1" ]
-then
+if [ -z "$1" ] ; then
 	echo "usage: $0 [-a] /path/to/destination"
 	exit 1
 fi
-if [ "$1" == "-a" ]
-then
+if [ "$1" == "-a" ] ; then
 	AUTOINSTALL="yes"
 	shift
 fi
 BACKUPDIR="$1"
-if [ -z "${BACKUPDIR}" ]
-then
+if [ -z "$BACKUPDIR" ] ; then
 	echo "No destination, aborted"
 	exit 1
 fi
+if [ ! -d "$BACKUPDIR" ] ; then
+	echo "$BACKUPDIR is not a directory, aborted"
+	exit 1
+fi
+
+
 BACKUPFILE=/usr/lib/enigma2/python/Plugins/Extensions/AutoBackup/backup.cfg
 USER_BACKUPFILE=/etc/backup.cfg
 USER_AUTOINSTALL=/etc/autoinstall
 INSTALLED=/etc/installed
 TEMP_INSTALLED=/tmp/installed
 RESTORE_TEMP=/tmp/restore.cfg
-MACADDR=`cat /sys/class/net/eth0/address | cut -b 1,2,4,5,7,8,10,11,13,14,16,17`
+MACADDR=`cat /sys/class/net/eth0/address | tr -d :`
+[ -z "$MACADDR" ] && MACADDR=nomac
 
-if [ -x /usr/bin/opkg ]
-then
-	IPKG=/usr/bin/opkg
-else
-	IPKG=ipkg
-fi
+type opkg >/dev/null  &&  IPKG=opkg  ||  IPKG=ipkg
 
-echo "Backup to ${BACKUPDIR}/backup/"
-mkdir -p "${BACKUPDIR}/backup"
+echo "Backup to $BACKUPDIR/backup/"
+[ ! -d "$BACKUPDIR/backup" ] && mkdir -p "$BACKUPDIR/backup"
 
-if [ -f ${BACKUPFILE} ]
-then
-	for file in $(cat ${BACKUPFILE})
-	do
-		if [ -f $file ]
-		then
-			echo $file >> ${RESTORE_TEMP}
-		else
-			if [ -d $file ]
-			then
-			echo $file >> ${RESTORE_TEMP}
-			fi
-		fi
-	done
-fi
+for bckfile in $BACKUPFILE $USER_BACKUPFILE ; do
+    if [ -f $bckfile ] ; then
+	while read file ; do
+		[ -f $file -o -d $file ] && echo $file >> $RESTORE_TEMP
+	done < $bckfile
+    fi
+done
 
-if [ -f ${USER_BACKUPFILE} ]
-then
-	for file in $(cat ${USER_BACKUPFILE})
-	do
-		if [ -f $file ]
-		then
-			echo $file >> ${RESTORE_TEMP}
-		else
-			if [ -d $file ]
-			then
-			echo $file >> ${RESTORE_TEMP}
-			fi
-		fi
-	done
-fi
+cp -f /etc/passwd /tmp && echo /tmp/passwd >> $RESTORE_TEMP
+[ -f /etc/shadow ] && cp -f /etc/shadow /tmp && echo /tmp/shadow >> $RESTORE_TEMP
 
-cp -f /etc/passwd /tmp && echo /tmp/passwd >> ${RESTORE_TEMP}
-[ -f /etc/shadow ] && cp -f /etc/shadow /tmp && echo /tmp/shadow >> ${RESTORE_TEMP}
+[ -f /etc/fstab ] && \
+    grep -E ' cifs | nfs | swap |^UUID=|^LABEL=' /etc/fstab > /tmp/fstab && \
+    echo /tmp/fstab >> $RESTORE_TEMP
 
-if [ -f /etc/fstab ]
-then
-  for fstype in ' cifs ' ' nfs ' ' swap ' '^UUID=' '^LABEL='
-  do
-    grep "${fstype}" /etc/fstab >> /tmp/fstab
-  done
-  [ -s /tmp/fstab ] && echo /tmp/fstab >> ${RESTORE_TEMP}
-fi
+crontab -l > /tmp/crontab 2> /dev/null && echo /tmp/crontab >> $RESTORE_TEMP
 
-crontab -l > /tmp/crontab 2> /dev/null && echo /tmp/crontab >> ${RESTORE_TEMP}
+tar -czf "$BACKUPDIR/backup/PLi-AutoBackup$MACADDR.tar.gz" --files-from=$RESTORE_TEMP 2> /dev/null
+ln -f -s PLi-AutoBackup$MACADDR.tar.gz "$BACKUPDIR/backup/PLi-AutoBackup.tar.gz" || \
+cp -p "$BACKUPDIR/backup/PLi-AutoBackup$MACADDR.tar.gz" "$BACKUPDIR/backup/PLi-AutoBackup.tar.gz"
 
-tar -czf "${BACKUPDIR}/backup/PLi-AutoBackup${MACADDR}.tar.gz" --files-from=/tmp/restore.cfg 2> /dev/null
-if [ ! -z "${MACADDR}" ]
-then
-	ln -f -s PLi-AutoBackup${MACADDR}.tar.gz "${BACKUPDIR}/backup/PLi-AutoBackup.tar.gz" || cp -p "${BACKUPDIR}/backup/PLi-AutoBackup${MACADDR}.tar.gz" "${BACKUPDIR}/backup/PLi-AutoBackup.tar.gz"
-fi
-
-if [ "${AUTOINSTALL}" == "yes" -a -f ${INSTALLED} ]
-then
-	echo "Generating ${BACKUPDIR}/backup/autoinstall${MACADDR}"
-	${IPKG} list_installed | cut -d ' ' -f 1 > ${TEMP_INSTALLED}
-	diff ${INSTALLED} ${TEMP_INSTALLED} | grep '+' | grep -v '@' | grep -v '+++' | sed 's/^+//' > "${BACKUPDIR}/backup/autoinstall${MACADDR}"
-	if [ -f ${USER_AUTOINSTALL} ]
-	then
-		for plugin in `cat ${USER_AUTOINSTALL}`
-		do
-			cp -p "${BACKUPDIR}/backup/autoinstall" /tmp/autoinstall
-			pluginname=`echo ${plugin} | sed 's/_.*//' | sed -re 's/^.+\///'`
-			cat /tmp/autoinstall | grep -v "$pluginname$" > "${BACKUPDIR}/backup/autoinstall${MACADDR}"
-		done
-		cat ${USER_AUTOINSTALL} >> "${BACKUPDIR}/backup/autoinstall${MACADDR}"
+if [ "$AUTOINSTALL" == "yes" -a -f $INSTALLED ] ; then
+	echo "Generating $BACKUPDIR/backup/autoinstall$MACADDR"
+	$IPKG list_installed | cut -d ' ' -f 1 > $TEMP_INSTALLED
+	diff $INSTALLED $TEMP_INSTALLED | grep "^+" | grep -v "^+++ $TEMP_INSTALLED" | \
+               sed 's/^+//' > "$BACKUPDIR/backup/autoinstall$MACADDR"
+	if [ -f $USER_AUTOINSTALL ] ; then
+		while read plugin ; do
+			pluginname=`echo $plugin | sed -e 's/_.*//' -re 's/^.+\///'`
+			mv "$BACKUPDIR/backup/autoinstall$MACADDR"  "$BACKUPDIR/backup/au.$$"
+			grep -v "$pluginname$" "$BACKUPDIR/backup/au.$$"  > "$BACKUPDIR/backup/autoinstall$MACADDR"
+		done < $USER_AUTOINSTALL
+		cat $USER_AUTOINSTALL >> "$BACKUPDIR/backup/autoinstall$MACADDR"
 	fi
-	if [ ! -z "${MACADDR}" ]
-	then
-		ln -f -s autoinstall${MACADDR} "${BACKUPDIR}/backup/autoinstall" || cp -p "${BACKUPDIR}/backup/autoinstall${MACADDR}" "${BACKUPDIR}/backup/autoinstall"
-	fi
+	ln -f -s autoinstall$MACADDR "$BACKUPDIR/backup/autoinstall" || \
+	cp -p "$BACKUPDIR/backup/autoinstall$MACADDR" "$BACKUPDIR/backup/autoinstall"
 fi
 
-touch "${BACKUPDIR}/backup/.timestamp"
+touch "$BACKUPDIR/backup/.timestamp"
 rm -f /tmp/restore.cfg
 rm -f /tmp/crontab
 rm -f /tmp/fstab
 rm -f /tmp/passwd
 rm -f /tmp/shadow
-rm -f ${TEMP_INSTALLED}
-true
+rm -f $TEMP_INSTALLED
+rm -f "$BACKUPDIR/backup/au.$$"
+
+exit 0
